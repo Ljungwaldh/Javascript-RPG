b6f67440a3215f429ac7ea595880aa16f1de56c1
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-07-04 10:55:14 +0000

    Completed ataack functions for both player and opponent. Currently building restart function for when a game is over

--- fight.js
+++ fight.js
@@ -10,13 +10,45 @@ let opponent = {
 
 const attack = () => {
     let attackButton = document.getElementById('attack-button');
+    let restartButton = document.getElementById('restart-button');
+    let gameMessage = document.getElementById('game-message');
 
-
-    let playerAttack = Math.floor(Math.random() * player.power);
+    let playerAttack = determineAttack(player.power);
     opponent.health -= playerAttack;
     printToScreen();
 
+    if (isGameOver(opponent.health)){
+        gameMessage.innerText = "Player Won Fight"
+        attackButton.hidden = true;
+        restartButton.hidden = false;
+        return;
+    }
+
     attackButton.disabled = true;
+    gameMessage.innerText = "Opponent is about to strike!"
+
+    setTimeout(() =>{
+    let opponentAttack = determineAttack(opponent.power);
+    player.health -= opponentAttack;
+    printToScreen();
+
+    if (isGameOver(player.health)){
+        gameMessage.innerText = "Opponent Won Fight"
+        attackButton.hidden = true;
+        restartButton.hidden = false;
+        return;
+    }
+    attackButton.disabled = false;
+    }, 250)
+    
+}
+
+const determineAttack = (power) => {
+    return Math.floor(Math.random() * power);
+}
+
+const isGameOver = (health) => {
+    return health <= 0;
 }
 
 const printToScreen = () => {
